e481699be4f3fa80a0620f2a860b4b6ebd9d79b6
Ruben Dominguez <argenis.dominguez@hotmail.com>
2025-10-28 17:31:55 -0400

    update: keybindings

--- lazyvim.sh
+++ lazyvim.sh
@@ -110,6 +110,8 @@ end, { noremap = true, silent = true })
 -- Buffer navigation
 vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gt", "gg", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gb", "G", { noremap = true, silent = true })
 
 -- Telescope keymaps
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
